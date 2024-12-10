9b65dd9346fc300bf148fc67b29eaedb112f94f6
SiteDecors <nschldhl61@gmail.com>
2024-12-10 07:52:41 +0545

    Changes for Backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,63 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { Pool } = require("pg");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
-app.use(cors()); 
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "MacBookAir",
+  port: 5432,
+});
+const SECRET_KEY = process.env.SECRET_KEY;
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get("/api/data/", (req, res) => {
-    res.send({"message": data});
+// signup code
+app.post("/signup", async (req, res) => {
+    const { username, email , password , role } = req.body;
+    if (username.trim().length === 0 || email.trim().length === 0 || password.trim().length === 0 || !["seller", "buyer"].includes(role)) {
+        return res.status(400).json({ message: "Fields can't be empty" });
+    }
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const existingUser = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+      if (existingUser.rows.length > 0) {
+        return res.status(400).json({ message: "Email is already in use" });
+      }
+      await pool.query(
+        "INSERT INTO users (username, password , email , role) VALUES ($1, $2 , $3  , $4)",
+        [username, hashedPassword , email , role]
+      );
+      res.status(201).json({ message: "Registered successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message : "Error registering user" });
+    }
 });
 
-app.listen(8000, () => {
-    console.log("Server started at port 8000");
-});
+// login code
+app.post("/login", async (req, res) => {
+    const {email , password} = req.body;
+    const user = await pool.query("Select * from users where email = $1",[email,])
+    if(user.rows.length === 0){
+        return res.status(404).json({message : "User doesnot exist"})
+    }
+    const isValidPassword = await bcrypt.compare(password , user.rows[0].password);
+    if(isValidPassword == true){
+        return res.status(200).json({message : "Login success"})
+    }else{
+        return res.status(404).json({message:"Invalid password"})
+    }
+  });
+
+
+
+app.listen(3008, () => {
+    console.log('Server is running on http://localhost:3008');
+  });
